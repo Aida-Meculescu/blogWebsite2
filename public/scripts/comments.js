300ae2058c272fd2c48a24aec1b2b35d1236b420
Aida-Meculescu <aida.meculescu@gmail.com>
2023-09-01 00:22:07 +0300

    ongoing...

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,5 +1,8 @@
 const loadCommentsBtnElement = document.getElementById('load-comments-btn')
 const commentsSectionElement = document.getElementById('comments')
+const commentsFormElement = document.querySelector('#comments-form form')
+const commentTitleElement = document.getElementById('title')
+const commentTextElement = document.getElementById('text')
 
 function createCommentsList(comments) {
     const commentListElement = document.createElement('ol')
@@ -30,4 +33,22 @@ async function fetchCommentsForPost() {
     commentsSectionElement.appendChild(commentsListElement)
 }
 
+function saveComment(event) {
+    event.preventDefault()
+    const postId = commentsFormElement.dataset.postid
+
+    const enterTitle = commentTitleElement.value
+    const enterText = commentTextElement.value
+
+    const comment = { title: enterTitle, text: enterTitle }
+
+    console.log(enterTitle, enterText)
+
+    fetch(`/posts/${postId}/comments`, {
+        method: 'POST',
+        body: JSON.stringify(comment)
+    })
+}
+
 loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost)
+commentsFormElement.addEventListener('submit', saveComment)
