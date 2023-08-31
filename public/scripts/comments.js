3da018bfca106a862cd05f04aeb6d9f17ba864a8
Aida-Meculescu <aida.meculescu@gmail.com>
2023-09-01 00:58:02 +0300

    ongoing ...

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,54 +1,53 @@
-const loadCommentsBtnElement = document.getElementById('load-comments-btn')
-const commentsSectionElement = document.getElementById('comments')
-const commentsFormElement = document.querySelector('#comments-form form')
-const commentTitleElement = document.getElementById('title')
-const commentTextElement = document.getElementById('text')
+const loadCommentsBtnElement = document.getElementById('load-comments-btn');
+const commentsSectionElement = document.getElementById('comments');
+const commentsFormElement = document.querySelector('#comments-form form');
+const commentTitleElement = document.getElementById('title');
+const commentTextElement = document.getElementById('text');
 
 function createCommentsList(comments) {
-    const commentListElement = document.createElement('ol')
+    const commentListElement = document.createElement('ol');
 
     for (const comment of comments) {
-        const commentElement = document.createElement('li')
+        const commentElement = document.createElement('li');
         commentElement.innerHTML = `
-            <article class="comment-item">
-                <h2>${comment.title}  </h2>
-                <p>${comment.text} </p>
-            </article>
-        `;
-        commentListElement.appendChild(commentElement)
+      <article class="comment-item">
+        <h2>${comment.title}</h2>
+        <p>${comment.text}</p>
+      </article>
+    `;
+        commentListElement.appendChild(commentElement);
     }
 
-    return commentListElement
+    return commentListElement;
 }
 
-
 async function fetchCommentsForPost() {
-    const postId = loadCommentsBtnElement.dataset.postid
-    const response = await fetch(`/posts/${postId}/comments`)
-    const responseData = await response.json()
-    // console.log(responseData)
+    const postId = loadCommentsBtnElement.dataset.postid;
+    const response = await fetch(`/posts/${postId}/comments`);
+    const responseData = await response.json();
 
-    const commentsListElement = createCommentsList(responseData)
-    commentsSectionElement.innerHTML = ''
-    commentsSectionElement.appendChild(commentsListElement)
+    const commentsListElement = createCommentsList(responseData);
+    commentsSectionElement.innerHTML = '';
+    commentsSectionElement.appendChild(commentsListElement);
 }
 
 function saveComment(event) {
-    event.preventDefault()
-    const postId = commentsFormElement.dataset.postid
+    event.preventDefault();
+    const postId = commentsFormElement.dataset.postid;
 
-    const enterTitle = commentTitleElement.value
-    const enterText = commentTextElement.value
+    const enteredTitle = commentTitleElement.value;
+    const enteredText = commentTextElement.value;
 
-    const comment = { title: enterTitle, text: enterTitle }
-
-    console.log(enterTitle, enterText)
+    const comment = { title: enteredTitle, text: enteredText };
 
     fetch(`/posts/${postId}/comments`, {
         method: 'POST',
-        body: JSON.stringify(comment)
-    })
+        body: JSON.stringify(comment),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
 
-loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost)
-commentsFormElement.addEventListener('submit', saveComment)
+loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
+commentsFormElement.addEventListener('submit', saveComment);
